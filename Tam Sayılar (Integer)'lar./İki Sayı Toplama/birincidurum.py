77bce282ff3b2f78a2ca2c9a4e014f55e2313bb9
Agah Emir <70206891+agahemir@users.noreply.github.com>
2021-03-20 20:00:29 +0300

    Create birincidurum.py

---
+++ Tam Sayılar (Integer)'lar./İki Sayı Toplama/birincidurum.py
@@ -0,0 +1,10 @@
+# Sayılarımız için olan değişkenleri tanımlıyoruz. 
+
+sayi1 = 15
+sayi2 = 12
+
+# "toplam" değişkeni, ikisinin toplamı. 
+toplam = sayi1 + sayi2
+
+# değerleri yazdıralım.
+print("{0} + {1} = {2}" .format(sayi1, sayi2, toplam)) 
